8b108879e1db7f0897da1e2551129901e4ab4d6e
Arlen Pan <arlenpan@gmail.com>
2017-07-29 15:09:54 -0700

    ui progress 1

--- src/App.js
+++ src/App.js
@@ -2,18 +2,47 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// MATERIAL UI COMPONENTS
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+
+// UI COMPONENTS
+import Header from './Header.js';
+import Track from './Track.js';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <MuiThemeProvider>
+        <div className="App">
+
+          <Header />
+
+          <div className="body-container">
+            <Track />
+            <Track />
+            <Track />
+            <Track />
+          </div>
+
+          <FloatingActionButton className="button-addtrack">
+            <ContentAdd />
+          </FloatingActionButton>
+
+          {/*
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+          */}
+
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </MuiThemeProvider>
     );
   }
 }
---
+++ src/Track.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Track extends Component {
+  render() {
+    return (
+      <div className="track-container">
+      </div>
+    );
+  }
+}
+
+export default Track;
