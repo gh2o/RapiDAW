7a8345b2e996ebf6ad0d37d7fa506392950c30a1
Arlen Pan <arlenpan@gmail.com>
2017-07-29 16:20:27 -0700

    working on track UI

--- src/Header.js
+++ src/Header.js
@@ -5,16 +5,23 @@ import './App.css';
 import {Toolbar, ToolbarTitle} from 'material-ui/Toolbar';
 import Paper from 'material-ui/Paper';
 import FontIcon from 'material-ui/FontIcon';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <Paper className="header-container" zDepth={3}>
-          <Toolbar className="header-toolbar">
-            <ToolbarTitle text="SONGNAME" />
-          </Toolbar>
-          
+      <Paper className="header-container" zDepth={3}>
+        <Toolbar className="header-toolbar">
+          <ToolbarTitle text="SONGNAME" />
+        </Toolbar>
+
+        <div className="header-subcontainer">
+          {/*<Paper className="header-addtrack" zDepth={2}>
+            <TextField className="header-addtrack-input" defaultValue="New Track" />
+            <FlatButton icon={<FontIcon className="material-icons header-trackcontrol-icon">add</FontIcon>}/>
+          </Paper>*/}
+
           <Paper className="header-trackcontrol" zDepth={2}>
             <FontIcon className="material-icons header-trackcontrol-icon">play_arrow</FontIcon>
             <FontIcon className="material-icons header-trackcontrol-icon">pause</FontIcon>
@@ -24,9 +31,9 @@ class Header extends Component {
               {/* track seeker here*/}
             </div>
           </Paper>
+        </div>
 
-        </Paper>
-      </div>
+      </Paper>
     );
   }
 }
