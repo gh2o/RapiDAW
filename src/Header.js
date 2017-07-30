d2f23d1670a7b9decbe78765416232849ee225d3
Arlen Pan <arlenpan@gmail.com>
2017-07-29 23:35:47 -0700

    header change

--- src/Header.js
+++ src/Header.js
@@ -12,8 +12,8 @@ class Header extends Component {
   constructor() {
     super();
     this.measure = [];
-    for (var i=0; i < 100; i++) {
-      if (i%3 == 0 && i != 0) {
+    for (var i=1; i < 100; i++) {
+      if (i%4 == 0 && i != 0) {
         this.measure.push((<div className="measure-cell thick"></div>));
       } else {
         this.measure.push((<div className="measure-cell"></div>));
