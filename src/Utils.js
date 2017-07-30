0d0d8bf9ad3510e60d9a420b78a17b3e6f45709c
Gavin Li <gavinli@thegavinli.com>
2017-07-30 03:20:38 -0700

    piano key naming

--- src/Utils.js
+++ src/Utils.js
@@ -4,4 +4,10 @@ function generateID() {
     return randomstring.generate(12);
 }
 
-export { generateID };
+function pitchToName(num) {
+    var octave   = Math.floor(num / 12);
+    var semitone = Math.floor(num % 12);
+    return ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'][semitone] + octave;
+}
+
+export { generateID, pitchToName };
