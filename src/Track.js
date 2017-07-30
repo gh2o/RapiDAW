b03b9c71c92201d4385a1bf7cd66facb669b6803
Gavin Li <gavinli@thegavinli.com>
2017-07-29 17:44:10 -0700

    add track delete

--- src/Track.js
+++ src/Track.js
@@ -1,3 +1,5 @@
+// vim: sw=2 ts=2
+
 import React, { Component } from 'react';
 import './Track.css';
 
@@ -5,8 +7,9 @@ class Track extends Component {
   render() {
     return (
       <div className="track-container">
-      	<div className="track-info">
-      		<p>{this.props.track.name}</p>
+        <div className="track-info">
+          <p>{this.props.track.name}</p>
+          <p><a onClick={() => this.props.trackDeleteClicked(this.props.track)}>DELETE</a></p>
       	</div>
       </div>
     );
