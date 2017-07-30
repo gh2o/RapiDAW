a8ba8fc25a59a99190413d81323aecbe10626f38
Arlen Pan <arlenpan@gmail.com>
2017-07-29 17:55:11 -0700

    track merge fix

--- src/Track.js
+++ src/Track.js
@@ -8,15 +8,9 @@ class Track extends Component {
     return (
       <div className="track-container">
         <div className="track-info">
-<<<<<<< HEAD
-            <p>{this.props.track.name}</p>
-            
-        </div>
-=======
           <p>{this.props.track.name}</p>
           <p><a onClick={() => this.props.trackDeleteClicked(this.props.track)}>DELETE</a></p>
       	</div>
->>>>>>> b03b9c71c92201d4385a1bf7cd66facb669b6803
       </div>
     );
   }
